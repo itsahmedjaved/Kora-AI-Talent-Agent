69c13d4f2fbc539199e171b683eba630d5ed2a50
Ahmed Javed <iamahmedjaved1@gmail.com>
2026-04-26 20:56:32 +0500

    Refactor next.config.js for serverExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['pdf-parse'],
-  },
+  serverExternalPackages: ['pdf-parse'],
 }
-module.exports = nextConfig
+
+module.exports = exports = nextConfig
